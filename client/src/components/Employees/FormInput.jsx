d17ec40f300b0a3e5500a988672529ffa20c9013
Grace <wangxiaotian2012@gmail.com>
2022-07-14 16:00:17 -0700

    Implement data update for controlled form

--- client/src/components/Employees/FormInput.jsx
+++ client/src/components/Employees/FormInput.jsx
@@ -4,28 +4,37 @@ import {
   FormLabel,
   Input} from "@chakra-ui/react";
 
-const FormInput = ({ employee, fieldName, headerText}) => {
+const FormInput = ({ employee, fieldName, headerText, setEmployeeData}) => {
+
   const defaultValue = employee ? employee[fieldName] : "";
   const [value, setValue] = useState(defaultValue);
 
-
   const handleChange = (event) => {
     setValue(event.target.value);
+    console.log("fieldName", fieldName);
+    setEmployeeData(prev => {
+      const updatedData = {...prev};
+      updatedData[fieldName] = event.target.value;
+      return updatedData;
+    });
   };
 
-  return (<FormControl>
-    <FormLabel
-      htmlFor={fieldName}
-    >
-      {headerText}
-    </FormLabel>
-    <Input
-      id={fieldName}
-      placeholder={headerText}
-      value={value}
-      onChange={handleChange}
-    />
-  </FormControl>);
+  return (
+    <FormControl isRequired>
+      <FormLabel
+        htmlFor={fieldName}
+      >
+        {headerText}
+      </FormLabel>
+      <Input
+        id={fieldName}
+        name={fieldName}
+        placeholder={headerText}
+        value={value}
+        onChange={handleChange}
+      />
+    </FormControl>
+  );
 };
 
 export default FormInput;
