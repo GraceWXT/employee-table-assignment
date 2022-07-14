874098ed319455386e3a397da3d625f818f1332e
Grace <wangxiaotian2012@gmail.com>
2022-07-14 11:53:13 -0700

    Add styling to employee row text and buttons

--- client/src/components/Employees/EmployeeRow.jsx
+++ client/src/components/Employees/EmployeeRow.jsx
@@ -2,13 +2,20 @@ import { Tr, Td, Button } from "@chakra-ui/react";
 
 const EmployeeRow = ({ employee, dataFields }) => {
 
-  const tds = dataFields.map(dataField => <Td>{employee[dataField]}</Td>);
+  const tds = dataFields.map(dataField => (
+    <Td
+      key={employee.firstName + employee.lastName + dataField}
+      fontWeight="semibold"
+    >
+      {employee[dataField]}
+    </Td>
+  ));
 
   return (
     <Tr>
       {tds}
-      <Td><Button>Edit</Button></Td>
-      <Td><Button>Delete</Button></Td>
+      <Td><Button variant="outline" colorScheme="orange">Edit</Button></Td>
+      <Td><Button variant="outline" colorScheme="red">Delete</Button></Td>
     </Tr>
   );
 };
