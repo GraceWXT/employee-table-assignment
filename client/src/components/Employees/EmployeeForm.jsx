0ea194534661a415f0c0074059f5b96815c1c5a8
Grace <wangxiaotian2012@gmail.com>
2022-07-14 15:59:54 -0700

    Implement a controlled form

--- client/src/components/Employees/EmployeeForm.jsx
+++ client/src/components/Employees/EmployeeForm.jsx
@@ -1,34 +1,56 @@
+import { useState } from "react";
 import {
   ModalBody,
   ModalFooter,
   Button,
-  FormControl,
-  FormLabel,
-  Input,
   VStack
 } from "@chakra-ui/react";
 import FormInput from "./FormInput";
 
-const EmployeeForm = ({ onClose, handleAdd, employee, dataFields, headerTexts }) => {
+const EmployeeForm = ({
+  onClose, handleAdd, handleEdit, employee, dataFields, headerTexts
+}) => {
+  const defaultData = (() => {
+    if (employee) return employee;
 
+    const defaultData = {};
+    for (let dataField of dataFields) {
+      defaultData[dataField] = "";
+    }
+    return defaultData;
+  })();
+  const [employeeData, setEmployeeData] = useState(defaultData);
   const formInputs = dataFields.map((fieldName, index) => (
-    <FormInput key={index} employee={employee} fieldName={fieldName} headerText={headerTexts[index]} />
+    <FormInput
+      key={index}
+      employee={employee}
+      fieldName={fieldName}
+      headerText={headerTexts[index]}
+      setEmployeeData={setEmployeeData}
+    />
   ));
+  console.log('employeeData :>> ', employeeData);
 
   return (
     <>
-      <ModalBody pb={6}>
-        <VStack spacing={5}>
-          {formInputs}
-        </VStack>
-      </ModalBody>
-      <ModalFooter>
-        <Button
-          colorScheme="blue" mr={3}>
+      <form>
+        <ModalBody pb={6}>
+          <VStack spacing={5}>
+            {formInputs}
+          </VStack>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            type="submit"
+            onClick={employee ? handleEdit : handleAdd}
+          >
           Save
-        </Button>
-        <Button onClick={onClose}>Cancel</Button>
-      </ModalFooter>
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </form>
     </>
   );
 };
