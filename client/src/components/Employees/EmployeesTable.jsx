f2eeaf16199e5bb6ddda447d747282eacd2ef4d0
Grace <wangxiaotian2012@gmail.com>
2022-07-14 11:41:12 -0700

    Add employee rows to the table

--- client/src/components/Employees/EmployeesTable.jsx
+++ client/src/components/Employees/EmployeesTable.jsx
@@ -1,11 +1,21 @@
 import { Table, Thead, Tbody } from "@chakra-ui/react";
+import EmployeeRow from "./EmployeeRow";
 import HeaderRow from "./HeaderRow";
 
 const EmployeesTable = ({ employees }) => {
 
-  const headerTexts = Object.keys(employees[0]).map(fieldName => (
+  const dataFields = Object.keys(employees[0]);
+  const headerTexts = dataFields.map(fieldName => (
     fieldName.replace(/([A-Z])/g, " $1")
       .replace(/^./, match => match.toUpperCase())
+  ));
+
+  const employeeRows = employees.map(employee => (
+    <EmployeeRow
+      key={employee.firstName + employee.lastName}
+      employee={employee}
+      dataFields={dataFields}
+    />
   ));
 
   return (
@@ -14,7 +24,7 @@ const EmployeesTable = ({ employees }) => {
         <HeaderRow headerTexts={headerTexts}/>
       </Thead>
       <Tbody>
-
+        {employeeRows}
       </Tbody>
     </Table>
   );
