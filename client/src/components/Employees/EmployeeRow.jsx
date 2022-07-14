19ed2455d91754a90cc468cdca37d7c8d466b973
Grace <wangxiaotian2012@gmail.com>
2022-07-14 14:51:36 -0700

    Format the currency. Add comments.

--- client/src/components/Employees/EmployeeRow.jsx
+++ client/src/components/Employees/EmployeeRow.jsx
@@ -1,21 +1,24 @@
-import { Tr, Td, Button, useDisclosure, ModalBody } from "@chakra-ui/react";
+import { Tr, Td } from "@chakra-ui/react";
 import DeleteModal from "./DeleteModal";
 import EditModal from "./EditModal";
 
 
 const EmployeeRow = ({ employee, dataFields, headerTexts }) => {
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
+  // Map over the data fields to create table cells for one employee
+  // Format the currency if necessary
   const tds = dataFields.map(dataField => (
     <Td
       key={employee.firstName + employee.lastName + dataField}
       fontWeight="semibold"
     >
-      {employee[dataField]}
+      {typeof employee[dataField] === "number" ?
+        employee[dataField].toLocaleString("en-CA", { style: "currency", currency: "CAD" }).slice(0, -3)
+        : employee[dataField]}
     </Td>
   ));
 
+  // End each row with edit and delete buttons
   return (
     <Tr>
       {tds}
