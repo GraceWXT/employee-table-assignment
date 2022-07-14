f8bb31ba6352040459cb75b8a6a793545fae30cc
Grace <wangxiaotian2012@gmail.com>
2022-07-13 23:03:27 -0700

    Implement the axios call to get employees data

--- client/src/components/Employees/Employees.jsx
+++ client/src/components/Employees/Employees.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "../../api/axios";
+
 import { Heading, VStack } from "@chakra-ui/react";
 
 import EmployeesTable from "./EmployeesTable";
 
 const Employees = () => {
+  const [employees, setEmployees] = useState([]);
+
+  // Get the employees data and set to state
+  useEffect(() => {
+    axios.get("/api/employees")
+      .then(response => {
+        setEmployees(response.data.employees);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <VStack>
       <Heading textTransform="uppercase" >Employees</Heading>
